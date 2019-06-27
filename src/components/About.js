87e51ba031893bd5bb8c71a39f327ca5b1c17a10
emmanuelmacharia <samuelmarsha@outlook.com>
2019-06-27 15:39:58 +0300

    added the about page, and included some styling; it isnt complete so dont judge yet

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,156 @@ import React from 'react'
 export default function About() {
     return (
         <div className="fullscreen">
-            my cv should be here
+            <section className="personal-statement">
+                <h3>Personal statement</h3>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero expedita sapiente voluptatem possimus voluptate perferendis modi natus mollitia ex deleniti quod sint odio, facere officiis debitis officia maiores velit quibusdam corporis a doloremque. Corrupti architecto maxime, est iure maiores aut cumque quisquam facilis. Reprehenderit ad necessitatibus quibusdam soluta ratione autem.
+            </section>
+            <hr />
+            <article className ="about">
+                <aside className="technical-skills">
+                    <h2>Technical skills</h2>
+                    <h3>Languages,Frameworks & Libraries</h3>
+                    <ul>
+                        <li>Python</li>
+                        <li>JavaScript</li>
+                        <li>Django</li>
+                        <li>Flask</li>
+                        <li>React and Redux</li>
+                        <li>JQuery</li>
+                        <li>Bootstrap</li>
+                        <li>SASS/SCSS</li>
+                        <li>Node</li>
+                        <li>Express</li>
+                        <li>HTML5</li>
+                        <li>CSS3</li>
+                        <li>SQL</li>
+                    </ul>
+                    <h3>Databases</h3>
+                    <ul>
+                        <li>Postgresql</li>
+                        <li>MySql</li>
+                        <li>MsSql</li>
+                        <li>Redis</li>
+                        <li>MongoDB</li>
+                    </ul>
+                    <h3>Development Skills</h3>
+                    <ul>
+                        <li>Full-Stack development</li>
+                        <li>MERN stack development</li>
+                        <li>Test Driven Development</li>
+                        <li>Object Oriented Programming</li>
+                        <li>Functional Programming</li>
+                        <li>API (REST)</li>
+                        <li>Search Engine Optimization</li>
+                        <li>Data Structures</li>
+                        <li>Algorithms</li>
+                        <li>Continuous Integration</li>
+                        <li>Git and Version Control</li>
+                        <li>Documentation</li>
+                        <li>Web UI design</li>
+                        <li>User Experience (UX) design</li>
+                    </ul>
+                </aside>
+                <section className="experience">
+                    <h2>Experience</h2>
+                    <h3>Geecko group | Digital Marketing Specialist, sales and customer service</h3>
+                    <ul>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam unde cupiditate vitae doloribus eligendi neque cum ipsum illum ipsa libero?
+                        </li>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam unde cupiditate vitae doloribus eligendi neque cum ipsum illum ipsa libero?
+                        </li>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam unde cupiditate vitae doloribus eligendi neque cum ipsum illum ipsa libero?
+                        </li>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam unde cupiditate vitae doloribus eligendi neque cum ipsum illum ipsa libero?
+                        </li>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam unde cupiditate vitae doloribus eligendi neque cum ipsum illum ipsa libero?
+                        </li>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam unde cupiditate vitae doloribus eligendi neque cum ipsum illum ipsa libero?
+                        </li>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam unde cupiditate vitae doloribus eligendi neque cum ipsum illum ipsa libero?
+                        </li>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam unde cupiditate vitae doloribus eligendi neque cum ipsum illum ipsa libero?
+                        </li>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam unde cupiditate vitae doloribus eligendi neque cum ipsum illum ipsa libero?
+                        </li>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam unde cupiditate vitae doloribus eligendi neque cum ipsum illum ipsa libero?
+                        </li>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam unde cupiditate vitae doloribus eligendi neque cum ipsum illum ipsa libero?
+                        </li>
+                    </ul>
+                    <h3>
+                        Real Plan consultants | Field Researcher
+                    </h3>
+                    <ul>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam unde cupiditate vitae doloribus eligendi neque cum ipsum illum ipsa libero?
+                        </li>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam unde cupiditate vitae doloribus eligendi neque cum ipsum illum ipsa libero?
+                        </li>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam unde cupiditate vitae doloribus eligendi neque cum ipsum illum ipsa libero?
+                        </li>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam unde cupiditate vitae doloribus eligendi neque cum ipsum illum ipsa libero?
+                        </li>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam unde cupiditate vitae doloribus eligendi neque cum ipsum illum ipsa libero?
+                        </li>
+                    </ul>
+                    <h3>
+                        Nairobi county government | Industrial Attache
+                    </h3>
+                    <ul>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam unde cupiditate vitae doloribus eligendi neque cum ipsum illum ipsa libero?
+                        </li>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam unde cupiditate vitae doloribus eligendi neque cum ipsum illum ipsa libero?
+                        </li>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam unde cupiditate vitae doloribus eligendi neque cum ipsum illum ipsa libero?
+                        </li>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam unde cupiditate vitae doloribus eligendi neque cum ipsum illum ipsa libero?
+                        </li>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam unde cupiditate vitae doloribus eligendi neque cum ipsum illum ipsa libero?
+                        </li>
+                    </ul>
+                </section>
+                <section className="education">
+                    <h2>Education</h2>
+                    <h3>Maseno University</h3>
+                    <p>Bachelor of Arts, Urban and Regional planning with IT </p>
+                    <h3>Maseno University</h3>
+                    <p>Bachelor of Arts, Urban and Regional planning with IT </p>
+
+                    <h2>Certifications</h2>
+                    <h3>FreeCodeCamp.org</h3>
+                    <ul>
+                        <li>Responsive Web Design</li>
+                        <li>Responsive Web Design</li>
+                        <li>Responsive Web Design</li>
+                        <li>Responsive Web Design</li>
+                        <li>Responsive Web Design</li>
+                        <li>Responsive Web Design</li>
+                    </ul>
+                    <h3>Google Skills for Africa</h3>
+                    <p>Fundamentals of Digital Marketing</p>
+                </section>
+            </article>
         </div>
     )
 }
