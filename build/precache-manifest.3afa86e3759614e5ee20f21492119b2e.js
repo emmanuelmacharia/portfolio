3aff50855956bd029bf657dbb16f8d76ea6412e6
emmanuelmacharia <samuelmarsha@outlook.com>
2019-07-19 15:47:43 +0300

    built the app; for deployment to azure

---
+++ build/precache-manifest.3afa86e3759614e5ee20f21492119b2e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48d0eb8dc696e3ffaf82c46b4e2fe311",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be56e1e70f7a1d05610e",
+    "url": "/static/css/2.43d354e1.chunk.css"
+  },
+  {
+    "revision": "c1188ceb8b920a1f54f2",
+    "url": "/static/css/main.c18cd467.chunk.css"
+  },
+  {
+    "revision": "be56e1e70f7a1d05610e",
+    "url": "/static/js/2.9dc70826.chunk.js"
+  },
+  {
+    "revision": "c1188ceb8b920a1f54f2",
+    "url": "/static/js/main.5e4724e9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "22e2f04ccbba15b4cea61514eb2bcccf",
+    "url": "/static/media/arrow.22e2f04c.svg"
+  },
+  {
+    "revision": "4d8b8d97ec9f6a576b7213f16a0f800a",
+    "url": "/static/media/programming-3201149_960_720.4d8b8d97.png"
+  }
+]);
