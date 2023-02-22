ba54a166d42b93b55b23d55d88eed0f66158ceee
emmanuel macharia <samuelmarsha@outlook.com>
2023-02-22 14:57:10 +0300

    change the copy for experience in my portfolio

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,38 +1,64 @@
-import React from 'react'
+import React from "react";
 
 // COMPONENTS
 
-import Projects from './Projects'
-import projects from './projectsData';
+import Projects from "./Projects";
+import projects from "./projectsData";
 
 export default function Home() {
-    
-    const projectComponent = projects.map(
-        project => 
-        <Projects key = {project.id} name = {project.name} description = {project.description} thumbnail = {project.thumbnail} github = {project.github} deployed = {project.deployed} website = {project.website} tags = {project.tags}/>
-        )
+  const projectComponent = projects.map((project) => (
+    <Projects
+      key={project.id}
+      name={project.name}
+      description={project.description}
+      thumbnail={project.thumbnail}
+      github={project.github}
+      deployed={project.deployed}
+      website={project.website}
+      tags={project.tags}
+    />
+  ));
 
-    return (
-
-        <div className="fullscreen">
-            <section className = "featured-section">
-                <p>2 years of professional experience in software development | possesses excellent technical aptitude.</p>
-                <p> Proficient in developing Responsive and Interactive websites | Skillful in HTML5, CSS3, SCSS, Javascript, jQuery, Angular and React </p>
-                <p>Hands on experience working with python; both django and flask | Sound understanding of databases, API, SQL queries, JSON</p>
-                <p>Capable of peformance and unit testing, optimiation, Documentation, Version Control and Deployment</p>
-
-            </section>
-            <section className= "Projects">
-                {projectComponent}
-            </section>
-            <section className = "link-buttons">
-                <div>
-                    <a href = "https://github.com/emmanuelmacharia" target = "_blank" rel = "noopener noreferrer">Click here, To see more projects</a>
-                </div>
-                <div>
-                    <a href = "/contact">Get in touch</a>
-                </div>
-            </section>
+  return (
+    <div className="fullscreen">
+      <section className="featured-section">
+        <p>
+          Over 5 years of professional experience in software development |
+          possesses excellent technical aptitude.
+        </p>
+        <p>
+          {" "}
+          Proficient in developing Responsive and Interactive websites |
+          Skillful in HTML5, CSS3, SCSS, Javascript, jQuery, Angular and React{" "}
+        </p>
+        <p>
+          Hands on experience working with python; both django and flask | Sound
+          understanding of databases, API, SQL queries, JSON
+        </p>
+        <p>
+          Skilled using Tensorflow, Pytorch and Scikit-Learn to implement deep
+          learning and machine learning algorithms
+        </p>
+        <p>
+          Capable of peformance and unit testing, optimiation, Documentation,
+          Version Control and Deployment
+        </p>
+      </section>
+      <section className="Projects">{projectComponent}</section>
+      <section className="link-buttons">
+        <div>
+          <a
+            href="https://github.com/emmanuelmacharia"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Click here, To see more projects
+          </a>
         </div>
-    )
+        <div>
+          <a href="/contact">Get in touch</a>
+        </div>
+      </section>
+    </div>
+  );
 }
